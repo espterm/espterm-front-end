22101cc000e69f33b388602bbd221a7dfe82c730
Ondřej Hruška <ondra@ondrovo.com>
2017-09-09 19:50:34 +0200

    fix build script to also build html

--- build.sh
+++ build.sh
@@ -3,20 +3,23 @@
 echo "Packing JS..."
 
 cat jssrc/chibi.js \
-	jssrc/keymaster.js \
-	jssrc/utils.js \
-	jssrc/modal.js \
-	jssrc/notif.js \
-	jssrc/appcommon.js \
-	jssrc/lang.js \
-	jssrc/wifi.js \
-	jssrc/term_* \
-	jssrc/term.js > js/app-full.js
+  jssrc/keymaster.js \
+  jssrc/utils.js \
+  jssrc/modal.js \
+  jssrc/notif.js \
+  jssrc/appcommon.js \
+  jssrc/lang.js \
+  jssrc/wifi.js \
+  jssrc/term_* \
+  jssrc/term.js > js/app-full.js
 
 yuicompressor js/app-full.js > js/app.js
 
 echo "Building CSS..."
 
 sass --style=compressed sass/app.scss css/app.css
+
+echo "Building HTML..."
+php ./build_html.php
 
 echo "ESPTerm front-end ready"
