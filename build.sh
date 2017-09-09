20165f5306fb68da55fb59270cbfb57af57d1c1b
Ondřej Hruška <ondra@ondrovo.com>
2017-09-09 19:36:31 +0200

    new build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Packing JS..."
+
+cat jssrc/chibi.js \
+	jssrc/keymaster.js \
+	jssrc/utils.js \
+	jssrc/modal.js \
+	jssrc/notif.js \
+	jssrc/appcommon.js \
+	jssrc/lang.js \
+	jssrc/wifi.js \
+	jssrc/term_* \
+	jssrc/term.js > js/app-full.js
+
+yuicompressor js/app-full.js > js/app.js
+
+echo "Building CSS..."
+
+sass --style=compressed sass/app.scss css/app.css
+
+echo "ESPTerm front-end ready"
