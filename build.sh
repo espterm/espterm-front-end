espterm/espterm-front-end

commit 2febe199e4e38d7ed76d7cbfc09e75b565330d71

Don't overwrite window.screen

jssrc/term.js
@@ -45,5 +45,5 @@ window.termInit = function (labels, theme) {
 
   if (window.attachDebugScreen) window.attachDebugScreen(screen)
 
-  window.screen = screen // for debugging
+  window.termScreen = screen // for debugging
 }

commit 1f07aa9886db3c79879197c47afc55a6923fd544

Fix what 2febe19 broke
I really should be double-checking commits

build.sh
@@ -13,8 +13,8 @@ npm run babel -- -o js/app.js --source-maps jssrc/lib \
     jssrc/wifi.js \
     jssrc/term_* \
     jssrc/debug_screen.js \
-    jssrc/term.js \
-    jssrc/soft_keyboard.js
+    jssrc/soft_keyboard.js \
+    jssrc/term.js
 
 echo 'Building CSS...'
 

jssrc/soft_keyboard.js
@@ -1,4 +1,4 @@
-$.ready(() => {
+window.initSoftKeyboard = function (screen) {
   const input = qs('#softkb-input')
   if (!input) return // abort, we're not on the terminal page
 
@@ -89,4 +89,4 @@ $.ready(() => {
   })
 
   screen.on('open-soft-keyboard', () => input.focus())
-})
+}

jssrc/term.js
@@ -43,6 +43,7 @@ window.termInit = function (labels, theme) {
     }
   }
 
+  window.initSoftKeyboard(screen)
   if (window.attachDebugScreen) window.attachDebugScreen(screen)
 
   window.termScreen = screen // for debugging