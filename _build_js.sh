f9b11bdc30a2acc1c0146b88645c1a5f9d3d4e44
cpsdqs <cpsdqs@gmail.com>
2017-09-30 20:20:44 +0200

    Pass build arguments to build_html and build_js

--- _build_js.sh
+++ _build_js.sh
@@ -3,7 +3,7 @@ source "_build_common.sh"
 
 mkdir -p out/js
 echo 'Generating lang.js...'
-php ./dump_js_lang.php
+php ./dump_js_lang.php $@
 
 echo 'Processing JS...'
 npm run webpack
--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ source "_build_common.sh"
 rm -fr out/*
 
 ./_build_css.sh
-./_build_js.sh
-./_build_html.sh
+./_build_js.sh $@
+./_build_html.sh $@
 ./_build_assets.sh
 
 echo 'ESPTerm front-end ready'
